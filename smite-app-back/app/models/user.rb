865f8e27daa962f2f610d4365ec416535e7e24a6
kellypowers <kpowers.nj@gmail.com>
2020-06-02 21:44:04 -0400

    user had many players that they can favorite

--- smite-app-back/app/models/user.rb
+++ smite-app-back/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
+    has_many :players, through: :user_players
+    # user can favorite players to view on their home page
 
     
     
