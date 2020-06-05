07eb89cce5795b8748f6636cf01ab8454697cd39
kellypowers <kpowers.nj@gmail.com>
2020-06-05 18:19:48 -0400

    adds God component, which gets passed each god as props

--- smite-app-front/src/components/gods/God.js
+++ smite-app-front/src/components/gods/God.js
@@ -4,13 +4,25 @@ class God extends Component {
 
   render() {
     // const { god } = this.props
-    console.log(this.props);
+    // console.log("props in god" + JSON.stringify(this.props.god));
+    console.log("props in god" + this.props.god.name);
     return (
       <div>
-        <li>
-          {/* {player.text} */}
-          {this.props.god}
-        </li>
+        <ul>
+        <li key={this.props.god.god_id}>
+             <h3>{this.props.god.name}</h3>
+             <img src={this.props.god.god_image}/>
+             {this.props.god.god_kind}
+             <br/>
+             Abilities: 
+             <ul>
+                 <li>{this.props.god.ability1} <img src={this.props.god.ability1_image} /> - {this.props.god.ability1_description}</li>
+                 <li>{this.props.god.ability2} <img src={this.props.god.ability2_image} />- {this.props.god.ability2_description}</li>
+                 <li>{this.props.god.ability3} <img src={this.props.god.ability3_image} />- {this.props.god.ability3_description}</li>
+                 <li>{this.props.god.ability4}<img src={this.props.god.ability4_image} /> - {this.props.god.ability4_description}</li>
+             </ul>
+             </li>
+        </ul>
       </div>
     );
   }
--- smite-app-front/src/components/gods/Gods.js
+++ smite-app-front/src/components/gods/Gods.js
@@ -3,32 +3,41 @@ import God from './God'
 
 
 class Gods extends Component {
+    renderGodList = () => {
+        return this.props.gods.map(god => 
+         (<God god={god} />))
+    }
   render() {
     console.log(`props in gods is ${JSON.stringify(this.props)}`);
     console.log(`props.gods in gods is ${JSON.stringify(this.props.gods)}`);
     // let gods = this.props.gods;
+    // const renderGodList = () => {
+    //     this.props.gods.map(god => 
+    //     <God champ={god} />)
+    // }
 
-    const godList = this.props.gods.map( god => {
-        // console.log(god)
-        return <li key={god.god_id}>
-            <h3>{god.name}</h3>
-            <img src={god.god_image}/>
-            {god.god_kind}
-            <br/>
-            Abilities: 
-            <ul>
-                <li>{god.ability1} <img src={god.ability1_image} /> - {god.ability1_description}</li>
-                <li>{god.ability2} <img src={god.ability2_image} />- {god.ability2_description}</li>
-                <li>{god.ability3} <img src={god.ability3_image} />- {god.ability3_description}</li>
-                <li>{god.ability4}<img src={god.ability4_image} /> - {god.ability4_description}</li>
-            </ul>
-            </li>
+    // const godList = this.props.gods.map( god => {
+    //     // console.log(god)
+    //     return <li key={god.god_id}>
+    //         <h3>{god.name}</h3>
+    //         <img src={god.god_image}/>
+    //         {god.god_kind}
+    //         <br/>
+    //         Abilities: 
+    //         <ul>
+    //             <li>{god.ability1} <img src={god.ability1_image} /> - {god.ability1_description}</li>
+    //             <li>{god.ability2} <img src={god.ability2_image} />- {god.ability2_description}</li>
+    //             <li>{god.ability3} <img src={god.ability3_image} />- {god.ability3_description}</li>
+    //             <li>{god.ability4}<img src={god.ability4_image} /> - {god.ability4_description}</li>
+    //         </ul>
+    //         </li>
    
-    })
+    // })
     // console.log(godList);
     return (
       <ul>
-        {godList}
+        {/* {godList} */}
+        {this.renderGodList()}
       </ul>
     );
   }
