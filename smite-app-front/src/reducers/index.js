3e652bb8766e4b95046053d62cd661d8d2cb889e
kellypowers <kpowers.nj@gmail.com>
2020-06-12 17:30:22 -0400

    adds reducers for match

--- smite-app-front/src/reducers/index.js
+++ smite-app-front/src/reducers/index.js
@@ -3,12 +3,14 @@ import playerReducer from './playerReducer';
 import itemsReducer from './itemsReducer'
 import godsReducer from './godsReducer';
 import buildsReducer from "./buildsReducer";
+import matchReducer from "./matchReducer";
  
 const rootReducer = combineReducers({
   gods: godsReducer,
   player: playerReducer,
   items: itemsReducer,
-  builds: buildsReducer
+  builds: buildsReducer,
+  match: matchReducer
 });
  
 export default rootReducer
---
+++ smite-app-front/src/reducers/matchReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+    loading: false,
+
+}
+
+function matchReducer(state = initialState, action) {
+    switch (action.type) {
+      case 'FIND_MATCH_DETAILS':
+        return {
+          loading: true
+        }
+
+      case  "ADD_MATCH_DETAILS": 
+        console.log("action match " + JSON.stringify(action.match));
+        return {
+          ...action.match,
+          loading: 'success'
+        }
+
+      case 'ADD_MATCH_DETAILS_FAILURE':
+        return {
+            ...state,
+            loading: false,
+            error: action.error
+        }
+
+      default:
+        return state;
+    }
+  }
+  export default matchReducer;
