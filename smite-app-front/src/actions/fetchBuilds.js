9f741b02721f6fd15f43573a916668ad738c41ba
kellypowers <kpowers.nj@gmail.com>
2020-06-30 10:39:14 -0400

    cleans up fetchBuilds.js spacing, removes comments

--- smite-app-front/src/actions/fetchBuilds.js
+++ smite-app-front/src/actions/fetchBuilds.js
@@ -12,61 +12,57 @@ export const DELETE_BUILD_FAILURE = 'DELETE_BUILD_FAILURE';
 
    
 export function fetchBuilds() {
-    return (dispatch) => {
-      dispatch({ type: 'START_ADDING_BUILDS_REQUEST' });
-      fetch('http://localhost:3000/god_builds')
-        .then(response => response.json())
-        .then(builds => dispatch({ type: 'ADD_BUILDS', builds }));
-    };
-  }
+  return (dispatch) => {
+    dispatch({ type: 'START_ADDING_BUILDS_REQUEST' });
+    fetch('http://localhost:3000/god_builds')
+      .then(response => response.json())
+      .then(builds => dispatch({ type: 'ADD_BUILDS', builds }));
+  };
+}
 
-  export function postBuild(build, ownProps) {
-    return (dispatch) => {
-        dispatch({ type: 'START_POST_BUILD_REQUEST' });
-        fetch('http://localhost:3000/god_builds', {
-            headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify({
-            build
-        })
-     })
-          .then(response => response.json())
-          .then(build => {
-            // console.log("ownProps.history " + JSON.stringify(ownProps));
-            dispatch({ type: 'POST_BUILD', build });
-            // ownProps.history.push(`/builds/${build.id}`)
-            // browserHistory.push(`/builds/${build.id}`)
-            return (window.location = `http://localhost:3001/builds/${build.id}`)
-          })
-          // .then(return history.push(`/builds/${build.id}`))
-          .catch(error => console.log(error.message))
-      };
-    }
+export function postBuild(build, ownProps) {
+  return (dispatch) => {
+    dispatch({ type: 'START_POST_BUILD_REQUEST' });
+    fetch('http://localhost:3000/god_builds', {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+        build
+      })
+    })
+    .then(response => response.json())
+    .then(build => {
+      dispatch({ type: 'POST_BUILD', build });
+      return (window.location = `http://localhost:3001/builds/${build.id}`)
+    })
+    .catch(error => console.log(error.message))
+  };
+}
   
-    export function deleteBuild(buildid) {
-      return (dispatch) => {
-          dispatch({ type: 'START_DELETING_BUILD_REQUEST' });
-          fetch(`http://localhost:3000/god_builds/${buildid}`, {
-              headers: {
-              "Content-Type": "application/json",
-              "Accept": "application/json"
-          },
-          method: "DELETE",
-          body: JSON.stringify({
-              buildid
-          })
-       })
-            .then(response => response.json())
-            .then(build => {
-              console.log("build is " + build)
-              dispatch({ type: 'DELETE_BUILD', build })
-            })
-            .catch(error => {
-              console.log(error.message)
-              dispatch({ type: 'DELETE_BUILD_FAILURE', error })
-            })
-        };
-      }
+export function deleteBuild(buildid) {
+  return (dispatch) => {
+    dispatch({ type: 'START_DELETING_BUILD_REQUEST' });
+    fetch(`http://localhost:3000/god_builds/${buildid}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      method: "DELETE",
+      body: JSON.stringify({
+        buildid
+      })
+    })
+    .then(response => response.json())
+    .then(build => {
+      console.log("build is " + build)
+      dispatch({ type: 'DELETE_BUILD', build })
+    })
+    .catch(error => {
+      console.log(error.message)
+      dispatch({ type: 'DELETE_BUILD_FAILURE', error })
+    })
+  };
+}
