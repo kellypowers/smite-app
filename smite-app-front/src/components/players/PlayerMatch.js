001e8647751b5bd0e8c4423608219858d3f5320d
kellypowers <kpowers.nj@gmail.com>
2020-10-07 17:41:11 -0400

    attempts to pull accordion out of player match

--- smite-app-front/src/components/players/PlayerMatch.js
+++ smite-app-front/src/components/players/PlayerMatch.js
@@ -1,24 +1,46 @@
 import React, { useState } from 'react';
 // import { connect } from 'react-redux';
 import { Link} from 'react-router-dom';
+import Accordion from '../navigation/Accordion'
 
 
-
-function PlayerMatch = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
+function PlayerMatch(props)  {
+    // const [isOpen, setIsOpen] = useState(false);
   // render() {
-    let god = this.props.gods.gods.find(g=> g.god_id == this.props.matches.GodId);
+    let god = props.gods.find(g=> g.god_id == this.props.matches.GodId);
     return (
+      
       <div  className="player-match">
           <br />
-          <div  onClick={() => setIsOpen(!isOpen)}className="accordion-header">
-            <h4> {this.props.matches.Win_Status} -- {this.props.matches.Map_Game} -- {this.props.matches.Match_Time} -- {this.props.matches.Minutes} minutes </h4>
-            <div className="accordion-indicator">
-              {isOpen ? '-' : '+'}
+          <Accordion 
+            title={`${props.matches.Win_Status} -- ${props.matches.Map_Game} -- ${props.matches.Match_Time} -- ${props.matches.Minutes} minutes`}
+            image={god.god_image}
+            body ={
+              <div className="player-match-deets">
+            <div className="player-match-god">
+            <h3><Link to={`/gods/${props.matches.GodId}`}>{props.matches.God}</Link></h3>
+            <br/>
+              <img className="match-god-image" src={god.god_image} alt={god.name}/>
+              <h5>K/D/A: {props.matches.Kills}/{props.matches.Deaths}/{props.matches.Assists}</h5>
+              <h5>Gold: {props.matches.Gold}</h5>
             </div>
-          </div>
-          {isOpen && (
-          <div className="player-match-deets">
+          <table className="player-match-table">
+            <tbody>
+              <tr><th>Damage: </th><td> {props.matches.Damage}</td></tr>
+              <tr><th>Damage Taken: </th> <td>{props.matches.Damage_Taken}</td></tr>
+              <tr><th>Structure Damage: </th> <td>{props.matches.Damage_Structure}</td></tr>
+              <tr><th>Healing: </th> <td>{props.matches.Healing}</td></tr>
+              <tr><th>Self Healing: </th> <td>{props.matches.Healing_Player_Self}</td></tr>
+              <tr><th>Wards Placed: </th> <td>{props.matches.Wards_Placed}</td></tr>
+            </tbody>
+          </table>
+          
+          <Link to={`/players/find/${props.portalid}/${props.playername}/${props.playerid}/player_matches/${props.matches.Match}`}>Get Match Details {this.props.player.Name} </Link>
+          </div>}
+            
+          />
+        
+          {/* <div className="player-match-deets">
             <div className="player-match-god">
             <h3><Link to={`/gods/${this.props.matches.GodId}`}>{this.props.matches.God}</Link></h3>
             <br/>
@@ -37,9 +59,8 @@ function PlayerMatch = (props) => {
             </tbody>
           </table>
           </div>
-          )}
-          <Link to={`/players/find/${props.routerProps.match.params.portalid}/${this.props.routerProps.match.params.playername}/${this.props.routerProps.match.params.playerid}/player_matches/${this.props.matches.Match}`}>Get Match Details {this.props.player.Name} </Link>
-          )}
+          <Link to={`/players/find/${props.routerProps.match.params.portalid}/${this.props.routerProps.match.params.playername}/${this.props.routerProps.match.params.playerid}/player_matches/${this.props.matches.Match}`}>Get Match Details {this.props.player.Name} </Link> */}
+          {/* )} */}
       </div>
     );
   // }
