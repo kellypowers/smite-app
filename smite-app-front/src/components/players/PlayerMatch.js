e3e87b9ee9afccc50f417f242dea9386d6c5f00c
kellypowers <kpowers.nj@gmail.com>
2020-10-07 18:36:57 -0400

    cleans up player match code, working accordion, horizontal table

--- smite-app-front/src/components/players/PlayerMatch.js
+++ smite-app-front/src/components/players/PlayerMatch.js
@@ -1,58 +1,58 @@
 import React, { useState } from 'react';
-// import { connect } from 'react-redux';
 import { Link} from 'react-router-dom';
-
-
 
 function PlayerMatch(props)  {
     const [isOpen, setIsOpen] = useState(false);
-  // render() {
     let god = props.gods.find(g=> g.god_id == props.matches.GodId);
     return (
       <div  className="player-match">
-          <br />
           <div  onClick={() => setIsOpen(!isOpen)}className="accordion-header">
-            <h4> {props.matches.Win_Status} -- {props.matches.Map_Game} -- {props.matches.Match_Time} -- {props.matches.Minutes} minutes </h4>
-            <div className="accordion-indicator">
+          <div className="accordion-indicator">
               {isOpen ? '-' : '+'}
             </div>
+            <h5> {props.matches.Win_Status} -- {god.name} -- {props.matches.Map_Game} -- {props.matches.Match_Time} -- {props.matches.Minutes} minutes </h5>
           </div>
           {isOpen && (
           <div className="player-match-deets">
             <div className="player-match-god">
             <h3><Link to={`/gods/${props.matches.GodId}`}>{props.matches.God}</Link></h3>
-            <br/>
               <img className="match-god-image" src={god.god_image} alt={god.name}/>
               <h5>K/D/A: {props.matches.Kills}/{props.matches.Deaths}/{props.matches.Assists}</h5>
-              <h5>Gold: {props.matches.Gold}</h5>
+              <h5>Level: {props.matches.Level}</h5>
             </div>
-          <table className="player-match-table">
-            <tbody>
-              <tr><th>Damage: </th><td> {props.matches.Damage}</td></tr>
-              <tr><th>Damage Taken: </th> <td>{props.matches.Damage_Taken}</td></tr>
-              <tr><th>Structure Damage: </th> <td>{props.matches.Damage_Structure}</td></tr>
-              <tr><th>Healing: </th> <td>{props.matches.Healing}</td></tr>
-              <tr><th>Self Healing: </th> <td>{props.matches.Healing_Player_Self}</td></tr>
-              <tr><th>Wards Placed: </th> <td>{props.matches.Wards_Placed}</td></tr>
-            </tbody>
-          </table>
-          <Link to={`/players/find/${props.portalid}/${props.playername}/${props.playerid}/player_matches/${props.matches.Match}`}>Get Match Details {props.player.Name} </Link>
+            <div className="player-match-deets-wrap">
+              <div className="wrap-table">
+              <table className="player-match-table">
+                <tbody>
+                  <tr>
+                    <th>Gold: </th>
+                    <th>Damage: </th>
+                    <th>Damage Taken: </th> 
+                    <th>Structure Damage: </th>
+                    <th>Healing: </th>
+                    <th>Self Healing: </th> 
+                    <th>Wards Placed: </th>
+                  </tr>
+                  <tr>
+                    <td>{props.matches.Gold}</td>
+                    <td> {props.matches.Damage}</td>
+                    <td>{props.matches.Damage_Taken}</td>
+                    <td>{props.matches.Damage_Structure}</td>
+                    <td>{props.matches.Healing}</td>
+                    <td>{props.matches.Healing_Player_Self}</td>
+                    <td>{props.matches.Wards_Placed}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="player-match-link">
+              <h4><Link to={`/players/find/${props.portalid}/${props.playername}/${props.playerid}/player_matches/${props.matches.Match}`}>View Match {props.player.Name} </Link></h4>
+            </div>
           </div>
-          )};
+          </div>
+          )}
       </div>
     );
-  // }
 };
 
-// const mapStateToProps = state => {
-//   return {
-//     gods: state.gods,
-//     items: state.items,
-//     player: state.player,
-//     playerMatches: state.playerMatches,
-//     match: state.match,
-//     clan: state.clan,
-//     loading: state.loading
-//   }
-// }
 export default PlayerMatch;
