534954b722c08864e8112b6d1bde82133b7f9bc7
kellypowers <kpowers.nj@gmail.com>
2020-10-07 19:10:15 -0400

    design God pagewith css elements

--- smite-app-front/src/components/gods/God.js
+++ smite-app-front/src/components/gods/God.js
@@ -20,37 +20,28 @@ const God = props => {
             <p>{props.god.god_kind}</p>
             <br />
           </div> 
-
-          <div id="god_play">
-             <table id="build-table">
+          <div className="god-play">
+             <table className="god-abilities-table">
                <tr>
                  <th colspan="4"><strong>Abilities</strong></th>
-               </tr>
-               <tr>
+                 </tr>
+                 <tr>
                  <th><img src={props.god.ability1_image} alt={props.god.ability1} />{props.god.ability1}</th>
-                 <td>{props.god.ability1_description}</td>
-               </tr>
-               <tr>
                  <th><img src={props.god.ability2_image} alt={props.god.ability2} />{props.god.ability2}</th>
-                 <td>{props.god.ability2_description}</td>
-               </tr>
-               <tr>
                  <th><img src={props.god.ability3_image} alt={props.god.ability3} />{props.god.ability3}</th>
-                 <td>{props.god.ability3_description}</td>
+                 <th><img src={props.god.ability4_image} alt={props.god.ability4} />{props.god.ability4}</th>
                </tr>
                <tr>
-                 <th><img src={props.god.ability4_image} alt={props.god.ability4} />{props.god.ability4}</th>
+                 <td>{props.god.ability1_description}</td>
+                 <td>{props.god.ability2_description}</td>
+                 <td>{props.god.ability3_description}</td>
                  <td>{props.god.ability4_description}</td>
                </tr>
-                 {/* <li> <img src={props.god.ability1_image} alt={props.god.ability1} />{props.god.ability1} - <br/> {props.god.ability1_description}</li> */}
-                 {/* <li><img src={props.god.ability2_image} alt={props.god.ability2} />{props.god.ability2} - <br/>{props.god.ability2_description}</li> */}
-                 {/* <li> <img src={props.god.ability3_image} alt={props.god.ability3} />{props.god.ability3} - <br/>{props.god.ability3_description}</li> */}
-                 {/* <li><img src={props.god.ability4_image} alt={props.god.ability4} />{props.god.ability4} - <br/>{props.god.ability4_description}</li> */}
              </table>
             </div>
-            <div id="god_lore">
+            <div className="god_lore">
               <h4>Lore: </h4>
-              <p>{props.god.lore}</p>
+              <p><em>{props.god.lore}</em></p>
             </div>
         {/* </ul> */}
       </div>
