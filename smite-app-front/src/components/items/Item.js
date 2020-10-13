88e92da6596fb8d85470d191dcbbae883ca2c3e7
kellypowers <kpowers.nj@gmail.com>
2020-10-13 16:50:35 -0400

    adds navbar to item page, makes it flex row

--- smite-app-front/src/components/items/Item.js
+++ smite-app-front/src/components/items/Item.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {Media} from 'react-bootstrap'
+import NavBar from '../navigation/NavBar'
 
 
 const Item = (props) => (
-  
-  <div className="item"   >
-    {console.log(JSON.stringify(props))}
-    <h3>{props.item.name}</h3>
-    <br/>
-    <Media> <img height={100} width={100} src={props.item.item_image} alt={props.item.name}/></Media>
-    <br/>
-    <p>Price:{props.item.price}</p>
-    <p>{props.item.short_description} </p>
-    <p>{props.item.item_secondary_description} </p>
-    <p>{props.item.item_stat} </p>
+  <div>
+    <NavBar/>
+    <div className="item-container"   >
+      {/* {console.log(JSON.stringify(props))} */}
+      <div className="item-header">
+        <h3>{props.item.name}</h3>
+        <br/>
+        <Media> <img height={100} width={100} src={props.item.item_image} alt={props.item.name}/></Media>
+        <br/>
+      </div>
+      <div className="item-info">
+        <p>Price: {props.item.price}</p>
+        <p>{props.item.short_description} </p>
+        <p>{props.item.item_secondary_description} </p>
+        <p>{props.item.item_stat} </p>
+      </div>
+    </div>
   </div>
 );
 
